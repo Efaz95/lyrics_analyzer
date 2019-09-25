58d9fc7555aacb435afe44a320245abe26499c4e
Efaz Khan <efazahmed@hotmail.com>
2019-09-25 14:08:57 -0400

    used env variables to store api

--- analyze_comments.py
+++ analyze_comments.py
@@ -1,15 +1,20 @@
+#this file will fetch some comments and do sentiment analysis on those
+import os
 import requests
 import json
 from bs4 import BeautifulSoup
 
-artist = "taylor swift"
-song = "love story"
+genius_api_token = os.environ['GENIUS_API']
 
-api_url = f"https://api.genius.com/search?q={artist}%20{song}&access_token=auvWU1kHsvKA2i7_bxEanYUEM5qyXi-mMVvDr1UVkPon0rrw8__AsH2dXqPjFvKY"
+artist = "maroon 5".strip()
+song = "sugar".strip()
+
+#getting song id using the api
+api_url = f"https://api.genius.com/search?q={artist}%20{song}&access_token={genius_api_token}"
 api_response = requests.get(api_url)
 id = api_response.json()['response']['hits'][0]['result']['id']
 
-
+#getting song comments
 url = f"https://genius.com/api/songs/{id}/comments?page=1&text_format=html,markdown"
 response = requests.get(url)
 comments = response.json()['response']['comments']
