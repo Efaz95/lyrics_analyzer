4b6e3901aad74c10669ed7ae6f1b295f29a6ebc2
Efaz Khan <efazahmed@hotmail.com>
2019-10-04 11:52:34 -0400

    added sentiment analysis

--- analyze_comments.py
+++ analyze_comments.py
@@ -3,11 +3,12 @@ import os
 import requests
 import json
 from bs4 import BeautifulSoup
+from textblob import TextBlob
 
 genius_api_token = os.environ['GENIUS_API']
 
-artist = "maroon 5".strip()
-song = "sugar".strip()
+artist = "justin beiber".strip()
+song = "baby".strip()
 
 #getting song id using the api
 api_url = f"https://api.genius.com/search?q={artist}%20{song}&access_token={genius_api_token}"
@@ -19,8 +20,10 @@ url = f"https://genius.com/api/songs/{id}/comments?page=1&text_format=html,markd
 response = requests.get(url)
 comments = response.json()['response']['comments']
 
+comment_list = []
 #prints top 10 user comments
 for i,comment in enumerate(comments,1):
-	print(f"{i}. {comment['body']['markdown']}")
-
-
+	comment_list.append(comment['body']['markdown'])
+	print(TextBlob(comment['body']['markdown']).sentiment.polarity, comment['body']['markdown'])
+	# print(comment['body']['markdown'])
+	# print(f"{i}. {comment['body']['markdown']}")
